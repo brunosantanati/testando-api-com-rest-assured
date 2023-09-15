e66ddc7d89ed78e1b61d52ae323387416b471af1
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-09-15 00:04:38 -0300

    Add primeiro teste com JUnit

---
+++ CursoRest/src/main/java/br/ce/wcaquino/rest/OlaMundoTest.java
@@ -0,0 +1,25 @@
+package br.ce.wcaquino.rest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+
+public class OlaMundoTest {
+	
+	@Test
+	public void testOlaMundo() {
+		Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
+		Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
+		Assert.assertTrue(response.statusCode() == 200);
+		Assert.assertTrue("O status code deveria ser 200", response.statusCode() == 200);
+		Assert.assertEquals(200, response.statusCode());
+		
+		ValidatableResponse validacao = response.then();
+		validacao.statusCode(200);
+	}
+
+}
