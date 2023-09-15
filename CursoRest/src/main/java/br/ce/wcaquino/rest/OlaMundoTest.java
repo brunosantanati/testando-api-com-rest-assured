50e719c5534ca3c3a4e9c1d8d3befe9ae7e8466e
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-09-15 00:33:22 -0300

    Exemplos de Matchers do Hamcrest

--- CursoRest/src/main/java/br/ce/wcaquino/rest/OlaMundoTest.java
+++ CursoRest/src/main/java/br/ce/wcaquino/rest/OlaMundoTest.java
@@ -1,13 +1,21 @@
 package br.ce.wcaquino.rest;
 
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.request;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
 
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
-
-import static io.restassured.RestAssured.*;
 
 public class OlaMundoTest {
 	
@@ -40,6 +48,28 @@ public class OlaMundoTest {
 		.then()
 			.assertThat() //Linha opcional, apenas para ficar mais claro
 			.statusCode(200);
+	}
+	
+	@Test
+	public void devoConhecerMatchersHamcrest() {
+		Assert.assertThat("Maria", Matchers.is("Maria"));
+		Assert.assertThat(128, Matchers.is(128));
+		Assert.assertThat(128, Matchers.isA(Integer.class));
+		Assert.assertThat(128d, Matchers.isA(Double.class));
+		Assert.assertThat(128d, Matchers.greaterThan(120d));
+		Assert.assertThat(128d, Matchers.lessThan(130d));
+		
+		List<Integer> impares = Arrays.asList(1, 3, 5, 7, 9);
+		assertThat(impares, hasSize(5));
+		assertThat(impares, contains(1, 3, 5, 7, 9));
+		assertThat(impares, containsInAnyOrder(1, 3, 5, 9, 7));
+		assertThat(impares, hasItem(1));
+		assertThat(impares, hasItems(1, 5));
+		
+		assertThat("Maria", is(not("João")));
+		assertThat("Maria", not("João"));
+		assertThat("Joaquina", anyOf(is("Maria"), is("Joaquina")));
+		assertThat("Joaquina", allOf(startsWith("Joa"), endsWith("ina"), containsString("qui")));
 	}
 
 }
