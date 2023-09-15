647ba5405bdf07a83b8d3be8adbcf796f21992a8
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-09-14 23:55:49 -0300

    Testar Status Code com Validacao do REST Assured

--- CursoRest/src/main/java/br/ce/wcaquino/rest/OlaMundo.java
+++ CursoRest/src/main/java/br/ce/wcaquino/rest/OlaMundo.java
@@ -3,6 +3,7 @@ package br.ce.wcaquino.rest;
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 
 public class OlaMundo {
 
@@ -10,8 +11,12 @@ public class OlaMundo {
 		Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
 		//Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me:80/ola");
 		//Response response = RestAssured.request(Method.GET, "http://50.116.38.228:80/ola");
-		System.out.println(response.getBody().asString());
-		System.out.println(response.statusCode());
+		System.out.println(response.getBody().asString().equals("Ola Mundo!"));
+		System.out.println(response.statusCode() == 200);
+		
+		ValidatableResponse validacao = response.then();
+		validacao.statusCode(200);
+		//validacao.statusCode(201); //AssertionError -> Expected status code <201> but was <200>
 	}
 
 }
