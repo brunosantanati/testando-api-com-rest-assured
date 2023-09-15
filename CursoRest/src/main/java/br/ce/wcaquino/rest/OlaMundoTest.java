0442b4fc2fa5539d5bd5eb83d7a8d9f22f77636c
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-09-15 00:37:34 -0300

    Validar o body

--- CursoRest/src/main/java/br/ce/wcaquino/rest/OlaMundoTest.java
+++ CursoRest/src/main/java/br/ce/wcaquino/rest/OlaMundoTest.java
@@ -71,5 +71,17 @@ public class OlaMundoTest {
 		assertThat("Joaquina", anyOf(is("Maria"), is("Joaquina")));
 		assertThat("Joaquina", allOf(startsWith("Joa"), endsWith("ina"), containsString("qui")));
 	}
+	
+	@Test
+	public void devoValidarBody() {
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/ola")
+		.then()
+			.statusCode(200)
+			.body(is("Ola Mundo!"))
+			.body(containsString("Mundo"))
+			.body(is(not(nullValue())));
+	}
 
 }
