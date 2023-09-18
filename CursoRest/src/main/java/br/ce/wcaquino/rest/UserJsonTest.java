3b5629b505d7171b6ab25ad23a714dc5d67a663c
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-09-17 23:31:45 -0300

    Verificacoes avancadas com REST Assured

--- CursoRest/src/main/java/br/ce/wcaquino/rest/UserJsonTest.java
+++ CursoRest/src/main/java/br/ce/wcaquino/rest/UserJsonTest.java
@@ -94,4 +94,30 @@ public class UserJsonTest {
 			.body("salary", contains(1234.5678f, 2500, null));
 	}
 
+	@Test
+	public void deveFazerVerificacoesAvancadas() {
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/users")
+		.then()
+			.statusCode(200)
+			.body("", hasSize(3))
+			.body("age.findAll{it <= 25}.size", is(2))
+			.body("age.findAll{it <= 25 && it > 20}.size", is(1))
+			.body("findAll{it.age <= 25 && it.age > 20}.name", hasItem("Maria Joaquina"))
+			.body("findAll{it.age <= 25}[0].name", is("Maria Joaquina"))
+			.body("findAll{it.age <= 25}[-1].name", is("Ana Júlia"))
+			.body("find{it.age <= 25}.name", is("Maria Joaquina"))
+			.body("findAll{it.name.contains('n')}.name", hasItems("Maria Joaquina", "Ana Júlia"))
+			.body("findAll{it.name.length() > 10}.name", hasItems("João da Silva", "Maria Joaquina"))
+			.body("name.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA"))
+			.body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA"))
+			.body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}.toArray()", 
+					allOf(arrayContaining("MARIA JOAQUINA"), arrayWithSize(1)))
+			.body("age.collect{it * 2}", hasItems(60, 50, 40))
+			.body("id.max()", is(3))
+			.body("salary.min()", is(1234.5678f))
+			.body("salary.findAll{it != null}.sum()", is(closeTo(3734.5678f, 0.001)))
+			.body("salary.findAll{it != null}.sum()", allOf(greaterThan(3000d), lessThan(5000d)));
+	}
 }
