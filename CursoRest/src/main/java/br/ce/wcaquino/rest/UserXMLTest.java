25d1b59dc0d068e572e87ecee9c5473c52699de1
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-10-05 12:27:58 -0300

    Exemplos usando XPath

--- CursoRest/src/main/java/br/ce/wcaquino/rest/UserXMLTest.java
+++ CursoRest/src/main/java/br/ce/wcaquino/rest/UserXMLTest.java
@@ -79,5 +79,28 @@ public class UserXMLTest {
 		Assert.assertEquals("Maria Joaquina".toUpperCase(), nomes.get(0).toString().toUpperCase());
 		Assert.assertTrue("ANA JULIA".equalsIgnoreCase(nomes.get(1).toString()));
 	}
+
+	@Test
+	public void devoFazerPesquisasAvancadasComXPath() {
+		given()
+		.when()
+			.get("https://restapi.wcaquino.me/usersXML")
+		.then()
+			.statusCode(200)
+			.body(hasXPath("count(/users/user)", is("3")))
+			.body(hasXPath("/users/user[@id = '1']"))
+			.body(hasXPath("//user[@id = '2']"))
+			.body(hasXPath("//name[text() = 'Luizinho']/../../name", is("Ana Julia")))
+			.body(hasXPath("//name[text() = 'Ana Julia']/following-sibling::filhos",
+					allOf(containsString("Zezinho"), containsString("Luizinho"))))
+			.body(hasXPath("/users/user/name", is("João da Silva")))
+			.body(hasXPath("//name", is("João da Silva")))
+			.body(hasXPath("/users/user[2]/name", is("Maria Joaquina")))
+			.body(hasXPath("/users/user[last()]/name", is("Ana Julia")))
+			.body(hasXPath("count(/users/user/name[contains(., 'n')])", is("2")))
+			.body(hasXPath("//user[age < 24]/name", is("Ana Julia")))
+			.body(hasXPath("//user[age > 20 and age < 30]/name", is("Maria Joaquina")))
+			.body(hasXPath("//user[age > 20][age < 30]/name", is("Maria Joaquina")));
+	}
 	
 }
