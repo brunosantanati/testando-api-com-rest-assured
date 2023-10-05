ffb43b4a29dfc555af62c58156ea34fc92c37d80
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-10-05 03:37:50 -0300

    Usar XML

---
+++ CursoRest/src/main/java/br/ce/wcaquino/rest/UserXMLTest.java
@@ -0,0 +1,26 @@
+package br.ce.wcaquino.rest;
+
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class UserXMLTest {
+
+	@Test
+	public void devoTrabalharComXML() {
+		given()
+		.when()
+			.get("https://restapi.wcaquino.me/usersXML/3")
+		.then()
+			.statusCode(200)
+			.body("user.name", is("Ana Julia"))
+			.body("user.@id", is("3"))
+			.body("user.filhos.name.size()", is(2))
+			.body("user.filhos.name[0]", is("Zezinho"))
+			.body("user.filhos.name[1]", is("Luizinho"))
+			.body("user.filhos.name", hasItem("Luizinho"))
+			.body("user.filhos.name", hasItems("Luizinho", "Zezinho"));
+	}
+	
+}
