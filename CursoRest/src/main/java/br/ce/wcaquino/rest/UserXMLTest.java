3fb256fcdc171aa738da9e7775f15cb420c898b7
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-10-05 04:01:28 -0300

    Pesquisas avancadas no XML

--- CursoRest/src/main/java/br/ce/wcaquino/rest/UserXMLTest.java
+++ CursoRest/src/main/java/br/ce/wcaquino/rest/UserXMLTest.java
@@ -44,5 +44,22 @@ public class UserXMLTest {
 			.body("name", hasItem("Luizinho"))
 			.body("name", hasItems("Luizinho", "Zezinho"));
 	}
+
+	@Test
+	public void devoFazerPesquisasAvancadasComXML() {
+		given()
+		.when()
+			.get("https://restapi.wcaquino.me/usersXML")
+		.then()
+			.statusCode(200)
+			.body("users.user.size()", is(3))
+			.body("users.user.findAll{it.age.toInteger() <= 25}.size()", is(2))
+			.body("users.user.@id", hasItems("1", "2", "3"))
+			.body("users.user.find{it.age == 25}.name", is("Maria Joaquina"))
+			.body("users.user.findAll{it.name.toString().contains('n')}.name", hasItems("Maria Joaquina", "Ana Julia"))
+			.body("users.user.salary.find{it != null}.toDouble()", is(1234.5678d))
+			.body("users.user.age.collect{it.toInteger() * 2}", hasItems(40, 50, 60))
+			.body("users.user.name.findAll{it.toString().startsWith('Maria')}.collect{it.toString().toUpperCase()}", is("MARIA JOAQUINA"));
+	}
 	
 }
