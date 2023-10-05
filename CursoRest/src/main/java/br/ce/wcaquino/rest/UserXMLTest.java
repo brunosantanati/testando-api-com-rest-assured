310a972abb5d321e72d51c93e3bae266d05ceacb
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-10-05 03:46:26 -0300

    Usar rootPath(), detachRootPath() e appendRootPath()

--- CursoRest/src/main/java/br/ce/wcaquino/rest/UserXMLTest.java
+++ CursoRest/src/main/java/br/ce/wcaquino/rest/UserXMLTest.java
@@ -21,6 +21,28 @@ public class UserXMLTest {
 			.body("user.filhos.name[1]", is("Luizinho"))
 			.body("user.filhos.name", hasItem("Luizinho"))
 			.body("user.filhos.name", hasItems("Luizinho", "Zezinho"));
+
+		// Reescrevendo o mesmo exemplo com algumas variações
+		given()
+		.when()
+			.get("https://restapi.wcaquino.me/usersXML/3")
+		.then()
+			.statusCode(200)
+
+			.rootPath("user")
+			.body("name", is("Ana Julia"))
+			.body("@id", is("3"))
+
+			.rootPath("user.filhos")
+			.body("name.size()", is(2))
+
+			.detachRootPath("filhos")
+			.body("filhos.name[0]", is("Zezinho"))
+			.body("filhos.name[1]", is("Luizinho"))
+
+			.appendRootPath("filhos")
+			.body("name", hasItem("Luizinho"))
+			.body("name", hasItems("Luizinho", "Zezinho"));
 	}
 	
 }
