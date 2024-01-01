b48299e5600ff3f214efe8faf92e8829bbbc2eab
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2024-01-01 18:10:52 -0300

    Usar parametros na URL

--- CursoRest/src/main/java/br/ce/wcaquino/rest/VerbosTest.java
+++ CursoRest/src/main/java/br/ce/wcaquino/rest/VerbosTest.java
@@ -71,4 +71,40 @@ public class VerbosTest {
                 .body("age", is(80))
                 .body("salary", is(1234.5678f));
     }
+
+    @Test
+    public void devoCustomizarURL() {
+        given()
+                .log().all()
+                .contentType("application/json")
+                .body("{ \"name\": \"Usuario alterado\", \"age\": 80 }")
+                .when()
+                .put("https://restapi.wcaquino.me/{entidade}/{userId}", "users", "1")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("id", is(1))
+                .body("name", is("Usuario alterado"))
+                .body("age", is(80))
+                .body("salary", is(1234.5678f));
+    }
+
+    @Test
+    public void devoCustomizarURLParte2() {
+        given()
+                .log().all()
+                .contentType("application/json")
+                .body("{ \"name\": \"Usuario alterado\", \"age\": 80 }")
+                .pathParam("entidade", "users")
+                .pathParam("userId", 1)
+                .when()
+                .put("https://restapi.wcaquino.me/{entidade}/{userId}")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("id", is(1))
+                .body("name", is("Usuario alterado"))
+                .body("age", is(80))
+                .body("salary", is(1234.5678f));
+    }
 }
