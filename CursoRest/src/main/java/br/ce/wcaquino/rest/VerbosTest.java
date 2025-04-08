6e3ded42fb211638be88560d1d68171c70e9fee0
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2025-04-08 01:39:22 -0300

    Serializar objeto

--- CursoRest/src/main/java/br/ce/wcaquino/rest/VerbosTest.java
+++ CursoRest/src/main/java/br/ce/wcaquino/rest/VerbosTest.java
@@ -46,6 +46,24 @@ public class VerbosTest {
             .body("name", is("Usuario via Map"))
             .body("age", is(25));
     }
+    
+    @Test
+    public void deveSalvarUsuarioUsandoObjeto() {
+    	User user = new User("Usuario via objeto", 35);
+
+        given()
+            .log().all()
+            .contentType("application/json")
+            .body(user)
+        .when()
+            .post("https://restapi.wcaquino.me/users")
+        .then()
+            .log().all()
+            .statusCode(201)
+            .body("id", is(notNullValue()))
+            .body("name", is("Usuario via objeto"))
+            .body("age", is(35));
+    }
 
     @Test
     public void naoDeveSalvarUsuarioSemNome() {
