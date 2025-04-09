17fe8bd8a47d8d64b28515c652076cfb8f4a1ce4
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2025-04-09 03:02:52 -0300

    Usar Query parametrizavel

--- CursoRest/src/main/java/br/ce/wcaquino/rest/EnvioDadosTest.java
+++ CursoRest/src/main/java/br/ce/wcaquino/rest/EnvioDadosTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import io.restassured.http.ContentType;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class EnvioDadosTest {
 	
@@ -18,6 +19,21 @@ public class EnvioDadosTest {
 			.log().all()
 			.statusCode(200)
 			.contentType(ContentType.JSON);
+	}
+	
+	@Test
+	public void deveEnviarValorViaQueryViaParam() {
+		given()
+			.log().all()
+			.queryParam("format", "xml")
+			.queryParam("outra", "coisa") //soh para mostrar como usa mais de 1 param, mas a applicacao nao fara nada com esse param
+		.when()
+			.get("https://restapi.wcaquino.me/v2/users")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.contentType(ContentType.XML)
+			.contentType(containsString("utf-8"));
 	}
 
 }
